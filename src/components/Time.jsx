d6b6463212446f6dc15a4e2250562bb42d60161b
catrt <rt990122@gmail.com>
2024-04-12 11:16:57 +0900

    Feat: 출퇴근 버튼 db와 연결하여 handling

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,23 +1,37 @@
 import '../styles/Time.scss';
 import React, { useEffect, useState } from 'react';
-
-const WorkButton = React.memo(({ inWork }) => {
-  return <button type="button">{inWork ? '퇴근 하기' : '출근 하기'}</button>;
-});
+import { db } from '../firebase';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 
 const Time = () => {
-  const [currentTime, setCurrentTime] = useState('00:00:00');
-  const [inWork, setInWork] = useState(false);
+  const [currentTime, setCurrentTime] = useState('-- : -- : --'); // 현재 시간
+  const [inWork, setInWork] = useState(false); // 출근 여부
+  const [outWork, setOutWork] = useState(false); // 퇴근 여부
 
+  // 한국 기준 Date 객체 반환
+  const getTimeInKorea = () => {
+    const date = new Date();
+    const timeInUtc = date.getTime() + date.getTimezoneOffset() * 60 * 1000;
+    const timeInKorea = new Date(timeInUtc + 9 * 60 * 60 * 1000);
+
+    return timeInKorea;
+  };
+
+  // Date 객체에서 'HH : MM : SS' 추출해서 반환
+  const getTimeString = (date) => {
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const timeString = hours + ' : ' + minutes + ' : ' + seconds;
+
+    return timeString;
+  };
+
+  // 한국 기준 현재 시간 timer
   useEffect(() => {
     const calculateCurrentTime = () => {
-      const date = new Date();
-      const timeInUtc = date.getTime() + date.getTimezoneOffset() * 60 * 1000;
-      const timeInKorea = new Date(timeInUtc + 9 * 60 * 60 * 1000);
-      const hours = String(timeInKorea.getHours()).padStart(2, '0');
-      const minutes = String(timeInKorea.getMinutes()).padStart(2, '0');
-      const seconds = String(timeInKorea.getSeconds()).padStart(2, '0');
-      const curTime = hours + ' : ' + minutes + ' : ' + seconds;
+      const timeInKorea = getTimeInKorea();
+      const curTime = getTimeString(timeInKorea);
 
       setCurrentTime(curTime);
     };
@@ -29,11 +43,61 @@ const Time = () => {
     };
   }, []);
 
+  // workDate가 이전 날짜인 경우 workTime 초기화
+  useEffect(() => {
+    const checkWorkDate = async () => {
+      const docRef = doc(db, 'workTime', 'wdPm9QLOre0i3GGpS5LC');
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const workTime = docSnap.data();
+        const timeInKorea = getTimeInKorea();
+        const currentDate = timeInKorea.toISOString().split('T')[0];
+
+        if (new Date(workTime.workDate) < new Date(currentDate)) {
+          await updateDoc(docRef, {
+            inWork: '-- : -- : --',
+            outWork: '-- : -- : --',
+            workDate: currentDate,
+          });
+          setInWork(false);
+          setOutWork(false);
+        }
+      }
+    };
+
+    checkWorkDate();
+  }, []);
+
+  // 출퇴근 버튼 클릭 시 workTime 수정 및 inWork, outWork 변경
+  const handleClick = async () => {
+    const docRef = doc(db, 'workTime', 'wdPm9QLOre0i3GGpS5LC');
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      if (!inWork) {
+        await updateDoc(docRef, {
+          inWork: currentTime,
+        });
+        setInWork(true);
+      }
+
+      if (inWork && !outWork) {
+        await updateDoc(docRef, {
+          outWork: currentTime,
+        });
+        setOutWork(true);
+      }
+    }
+  };
+
   return (
     <section className="time">
       <h3>현재 시각</h3>
       <div className="current-time">{currentTime}</div>
-      <WorkButton inWork={inWork} />
+      <button type="button" onClick={handleClick} disabled={inWork && outWork}>
+        {inWork ? (outWork ? '퇴근 완료' : '퇴근 하기') : '출근하기'}
+      </button>
     </section>
   );
 };
