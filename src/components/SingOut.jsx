f7dcaaf21cc5079708b5b5e823d85e1ee1d867a8
Kim Yeojin <duwls1503@gmail.com>
2024-04-15 10:50:28 +0900

    feature: 기능개발

--- src/components/SingOut.jsx
+++ src/components/SingOut.jsx
@@ -1,7 +1,22 @@
+import { getAuth, signOut } from 'firebase/auth';
 import React from 'react';
 
 const SingOut = () => {
-  return <div>SingOut</div>;
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const logOutClick = async () => {
+    try {
+      await signOut(auth);
+      alert('로그아웃 되었습니다.');
+      navigate('/login');
+    } catch (error) {
+      console.log(error.code);
+      console.log(error.message);
+      alert('로그아웃에 실패했습니다.');
+    }
+  };
+
+  return <button onClick={logOutClick}>Log Out</button>;
 };
 
 export default SingOut;
