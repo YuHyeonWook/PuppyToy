5cc148856b48bdde30fcdbd8d8458b0556c44b9e
catrt <rt990122@gmail.com>
2024-04-16 18:27:33 +0900

    Design: user profile 배경 추가 및 context 아이콘 추가

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react';
 import '../styles/UserProfile.scss';
+import { useEffect, useState } from 'react';
 import React from 'react';
 import Header from '../components/Header';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../firebase';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { FaDog, FaBone, FaBookReader, FaRegClock, FaClock } from 'react-icons/fa';
+import { ImManWoman } from 'react-icons/im';
+import userProfileBackgroundImage from '../assets/user_profile_background_image.jpg';
 
 const UserProfile = () => {
   const [userData, setUserData] = useState(null);
@@ -51,9 +54,9 @@ const UserProfile = () => {
       {userData ? (
         <div className="user-profile">
           <header>
-            <div className="user-profile__background">
-              <img src="" alt="User Profile BG" />
-            </div>
+            <div
+              className="user-profile__background"
+              style={{ backgroundImage: `url(${userProfileBackgroundImage})` }}></div>
             <div className="user-profile__image">
               <img src={userData.imageUrl} alt="User Profile Image" />
             </div>
@@ -61,27 +64,69 @@ const UserProfile = () => {
           </header>
           <main>
             <div className="user-profile__breed">
-              <p className="key">{'품종'}</p>
+              <p className="key">
+                <FaDog
+                  style={{
+                    color: '#000',
+                  }}
+                />
+                {'품종'}
+              </p>
               <p className="value">{userData.breed}</p>
             </div>
             <div className="user-profile__gender">
-              <p className="key">{'성별'}</p>
+              <p className="key">
+                <ImManWoman
+                  style={{
+                    color: '#000',
+                  }}
+                />
+                {'성별'}
+              </p>
               <p className="value">{userData.gender}</p>
             </div>
             <div className="user-profile__age">
-              <p className="key">{'나이'}</p>
-              <p className="value">{userData.age}</p>
+              <p className="key">
+                <FaBone
+                  style={{
+                    color: '#000',
+                  }}
+                />
+                {'나이'}
+              </p>
+              <p className="value">{userData.age}살</p>
             </div>
             <div className="user-profile__position">
-              <p className="key">{'직책'}</p>
+              <p className="key">
+                <FaBookReader
+                  style={{
+                    color: '#000',
+                  }}
+                />
+                {'직책'}
+              </p>
               <p className="value">{userData.position}</p>
             </div>
             <div className="user-profile__time-in-work">
-              <p className="key">{'출근 시간'}</p>
+              <p className="key">
+                <FaRegClock
+                  style={{
+                    color: '#000',
+                  }}
+                />
+                {'출근 시간'}
+              </p>
               <p className="value">{userData.inWork}</p>
             </div>
             <div className="user-profile__time-out-work">
-              <p className="key">{'퇴근 시간'}</p>
+              <p className="key">
+                <FaClock
+                  style={{
+                    color: '#000',
+                  }}
+                />
+                {'퇴근 시간'}
+              </p>
               <p className="value">{userData.outWork}</p>
             </div>
           </main>
