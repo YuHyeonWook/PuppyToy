8bb938e6d80762d05b3aec17e6775822be2a122e
catrt <rt990122@gmail.com>
2024-04-17 17:04:12 +0900

    Design: dog 컴포넌트 스타일 변경

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -4,19 +4,36 @@ import React from 'react';
 const Dog = ({ dog }) => {
   return (
     <div className="dog">
-      <img className="dog__image" src={dog.url} alt={dog.breeds[0]?.name} />
+      <div className="dog__image">
+        <img src={dog.url} alt={dog.breeds[0]?.name} />
+      </div>
       <div className="dog__info">
-        <h3>품종: {dog.breeds[0]?.name || '알 수 없음'}</h3>
-        <p>성격: {dog.breeds[0]?.temperament || '알 수 없음'}</p>
-        <p>평균 수명: {dog.breeds[0]?.life_span || '알 수 없음'}</p>
-        <p>
-          무게:{' '}
-          {dog.breeds[0]?.weight.imperial ? `${dog.breeds[0]?.weight.imperial} kg` : '알 수 없음'}{' '}
-        </p>
-        <p>
-          키:{' '}
-          {dog.breeds[0]?.height.imperial ? `${dog.breeds[0]?.height.imperial} 인치` : '알 수 없음'}
-        </p>
+        <h3>{dog.breeds[0]?.name || '알 수 없음'}</h3>
+        <div>
+          <p className="key">성격</p>
+          <p className="value">{dog.breeds[0]?.temperament || '알 수 없음'}</p>
+        </div>
+        <div>
+          <p className="key">평균 수명</p>
+          <p className="value">{dog.breeds[0]?.life_span || '알 수 없음'}</p>
+        </div>
+        <div>
+          <p className="key">무게</p>
+          <p className="value">
+            {' '}
+            {dog.breeds[0]?.weight.imperial
+              ? `${dog.breeds[0]?.weight.imperial} kg`
+              : '알 수 없음'}{' '}
+          </p>
+        </div>
+        <div>
+          <p className="key">키</p>
+          <p className="value">
+            {dog.breeds[0]?.height.imperial
+              ? `${dog.breeds[0]?.height.imperial} 인치`
+              : '알 수 없음'}
+          </p>
+        </div>
       </div>
     </div>
   );
