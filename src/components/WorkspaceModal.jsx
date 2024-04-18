6d8488e846b79ce5c8b3691382b8c348c04d0e51
YuHyeonWook <yho7955@naver.com>
2024-04-18 19:47:40 +0900

    design: 근무 신청 모달 flex 적용

--- src/components/WorkspaceModal.jsx
+++ src/components/WorkspaceModal.jsx
@@ -3,8 +3,10 @@ import '../styles/WorkspaceModal.scss';
 import DatePicker from 'react-datepicker';
 import ko from 'date-fns/locale/ko';
 import Select from 'react-select';
-import 'react-datepicker/dist/react-datepicker.css';
 import { getFirestore, addDoc, collection } from 'firebase/firestore';
+import { IoMdClose } from 'react-icons/io';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 const WorkspaceModal = ({ setModalOpen, selectedItem, setSelectedItem, readonly }) => {
   const [schedule, setSchedule] = useState(new Date());
@@ -72,37 +74,39 @@ const WorkspaceModal = ({ setModalOpen, selectedItem, setSelectedItem, readonly 
             setSelectedItem('');
             setModalOpen(false);
           }}>
-          X
+          <IoMdClose />
         </span>
         <div className="modal__conent">
-          <input className="modal__name" type="text" value={userItem.name} readOnly />
-          <Select
-            className="modal__select"
-            options={options}
-            placeholder="결석형태"
-            onChange={(e) => {
-              setAttendance(e.value);
-              setSelectOption(e);
-            }}
-            readOnly={readonly}
-            value={selectOption}
-            theme={(theme) => ({
-              ...theme,
-              borderRadius: 10,
-              colors: { ...theme.colors, primary25: '#e9deff', primary: '#c3a3ff' },
-            })}
-            isDisabled={readonly}
-          />
-          <DatePicker
-            showIcon
-            locale={ko}
-            dateFormat="yyyy년 MM월 dd일"
-            minDate={new Date()}
-            className="modal__date"
-            selected={schedule}
-            onChange={(date) => setSchedule(date)}
-            readOnly={readonly}
-          />
+          <div className="modal__wrapper">
+            <input className="modal__name" type="text" value={userItem.name} readOnly />
+            <Select
+              className="modal__select"
+              options={options}
+              placeholder="결석형태"
+              onChange={(e) => {
+                setAttendance(e.value);
+                setSelectOption(e);
+              }}
+              readOnly={readonly}
+              value={selectOption}
+              theme={(theme) => ({
+                ...theme,
+                borderRadius: 10,
+                colors: { ...theme.colors, primary25: '#e9deff', primary: '#c3a3ff' },
+              })}
+              isDisabled={readonly}
+            />
+            <DatePicker
+              showIcon
+              locale={ko}
+              dateFormat="yyyy년 MM월 dd일"
+              minDate={new Date()}
+              className="modal__date"
+              selected={schedule}
+              onChange={(date) => setSchedule(date)}
+              readOnly={readonly}
+            />
+          </div>
           <textarea
             name="reason"
             id="modal__reason"
