a12a2e91ec6f724b9e2f5b2a669dce38b3cd006e
YuHyeonWook <yho7955@naver.com>
2024-05-02 19:10:02 +0900

    chore: @types/react-datepicker 설치

--- src/components/WorkspaceModal.tsx
+++ src/components/WorkspaceModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import ko from 'date-fns/locale/ko';
 import Select from 'react-select';
